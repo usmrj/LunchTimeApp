3972838cd812fd5b221e8f074179d7ff51508bb8
Michal <m.kujawski2255@gmail.com>
2023-10-21 11:55:08 +0200

    simple connection- not done yet

--- Database.cpp
+++ Database.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QSqlQuery>
+#include <exception>
+#include <memory>
 
 //fully static class
 
@@ -8,6 +10,21 @@ class Database {
 private:
     Database() { }
 
+    QSqlDatabase makeConnection()
+    {
+//        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+//        db.setHostName("bigblue");
+//        db.setDatabaseName("flightdb");
+//        db.setUserName("acarlson");
+//        db.setPassword("1uTbSbAs");
+//        if(db.open())
+//        {
+//            return db;
+//        }
+//        else
+//        {
 
+//        }
+    }
 public:
 };
